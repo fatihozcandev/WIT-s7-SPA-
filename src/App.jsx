b93d82bff840a8b9018285212b357fcce7e9acfc
fatihozcandev <fatih-ozcan@windowslive.com>
2024-03-10 21:42:51 +0100

    gruplama

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import workintech from '/workintech.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import workintech from "/workintech.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
+        <a
+          href="https://github.com/Workintech/fsweb-s7-challenge-pizza"
+          target="_blank"
+        >
           <img src={workintech} className="logo" alt="Workintech logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -29,7 +32,7 @@ function App() {
         Click on the Workintech or Pizza logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
