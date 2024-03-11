0b8ac2b6e56259583ab97e9ae02fdacf67e36705
fatihozcandev <fatih-ozcan@windowslive.com>
2024-03-11 01:43:39 +0100

    Sipariş header eklendi

---
+++ src/components/Siparis/Siparis.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SiparisHeader from "./SiparisHeader";
+
+export default function Siparis() {
+  return (
+    <div>
+      <SiparisHeader />
+      <section></section>
+      <section></section>
+    </div>
+  );
+}
---
+++ src/components/Siparis/SiparisHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SiparisHeader.css";
+
+export default function SiparisHeader() {
+  return (
+    <header className="siparis-header">
+      <h1>TEKNOLOJİK YEMEKLER</h1>
+      <p>
+        Anasayfa - Seçenekler -<span id="secili-bolum"> Deneme</span>
+      </p>
+    </header>
+  );
+}
